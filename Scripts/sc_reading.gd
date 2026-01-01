94635d8c57742aa97ae3a8c6ccef2f50c1be0a23
Henry Kinoshita <42702870+eeeeeeri@users.noreply.github.com>
2026-01-01 00:43:06 -0300

    feat: superchat minigame

--- Scripts/sc_reading.gd
+++ Scripts/sc_reading.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
-@onready var color_rect: ColorRect = $ColorRect
+const SUPERCHAT = preload("uid://d16ukgas0boul")
+
+var superchats = []
+var sc_num = 0
+var read = 0
+
+func _ready() -> void:
+	sc_num = randi_range(3,8)
+	
+	for i in range(sc_num):
+		var new_superchat := SUPERCHAT.instantiate()
+		new_superchat.position.y = (sc_num - i - 1) * -36
+		add_child(new_superchat)
+		superchats.append(new_superchat)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("left_mouse"):
-		color_rect.color = Color.GREEN
-		Globals.last_minigame_won = true
+		read += 1
+		if read >= sc_num:
+			Globals.last_minigame_won = true
---
+++ Scripts/superchat.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+const usernames = [
+	"@eeeeeeeeeeri",
+	"@Perks",
+	"@gogibahp",
+	"@isaidmaybe",
+	"@EatingMikeTysonsAss",
+]
+
+const messages = [
+	"I miss holoen",
+	"I love your streams Kronii!",
+	"Otsu~!",
+	"Have a great stream!",
+	"Have my life savings",
+	"I run out of ideas"
+]
+
+@onready var username: Label = $Username
+@onready var message: Label = $Message
+
+func _ready() -> void:
+	frame = randi_range(0,4)
+	username.text = usernames.pick_random()
+	message.text = messages.pick_random()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("left_mouse"):
+		position.y += 36
