5969f45cdc8d12e1762a238bbf897ecf85514f0c
Henry Kinoshita <42702870+eeeeeeri@users.noreply.github.com>
2026-01-03 23:57:34 -0300

    feat: spelling bee minigame

---
+++ Scripts/spelling_bee.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+@onready var word_label: Label = $Word
+@onready var anwser: Label = $Anwser
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var confetti: CPUParticles2D = $Confetti
+
+const words = [
+	"SUPERCALIFRAGILISTICEXPIALIDOCIOUS",
+	"CODEPENDENCY",
+	"FETTUCCINE",
+	"DECAFFEINATED",
+	"BROCHURE",
+	"ELLIPTICAL",
+	"REFRIGERATOR",
+	"CONNOISSEUR",
+	"PTERODACTYL",
+	"SILHOUETTE",
+	"DOPPELGANGER",
+	"ARCHIPELAGO",
+	"ACACIA",
+	"DISCOMBOBULATE",
+	"HEMORRHOID",
+	"AQUEDUCT",
+	"AFFIDAVIT"
+]
+
+var word := ""
+var typed := ""
+var missed := false
+
+func _ready() -> void:
+	word = words.pick_random()
+	word_label.text = word
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and not event.is_pressed():
+		var ch = event.as_text_keycode()
+		if ch.length() == 1 and !Globals.last_minigame_won:
+			typed += ch
+			anwser.text = typed
+			if !word.begins_with(typed) and !missed:
+				animation_player.play("miss")
+				missed = true
+			if word == typed:
+				Globals.last_minigame_won = true
+				anwser.modulate = Color.GREEN
+				confetti.emitting = true
