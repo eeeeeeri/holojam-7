4d35747ae156898479596225096d8b1494619d60
Henry Kinoshita <42702870+eeeeeeri@users.noreply.github.com>
2026-01-10 19:47:35 -0300

    driving minigame

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -10,6 +10,7 @@ var minigames = [
 	Minigame.minigame("res://Scenes/Minigames/AddressingAllegations/addressing_allegations.tscn", "Adressing the Allegations","Spot the false allegations"),
 	Minigame.minigame("res://Scenes/Minigames/OutfitReveal/outfit_reveal.tscn", "New Outfit Reveal","Showcase your new outfit!"),
 	Minigame.minigame("res://Scenes/Minigames/Subway/subway.tscn", "Eat Subway","Devour it whole"),
+	Minigame.minigame("res://Scenes/Minigames/DrivingInMyCar/driving_in_my_car.tscn", "Don't crash","Driving in my car right after a stream"),
 ]
 
 var week_minigames = []
@@ -21,3 +22,4 @@ var current_minigame : Minigame
 var tasks_left : int = 20
 var days_left : int = 30
 var today := WeekDay.MON
+var cam_x : float
---
+++ Scripts/obstacle.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed := 2.0
+@export var car_speed := 8.0
+@export var limit := 144.0
+@export var _x : float
+
+func _process(delta: float) -> void:
+	
+	var distance = inverse_lerp(64, 144, position.y)
+	scale = Vector2(distance,distance) * 5
+	
+	position.y += distance * speed
+	position.x = 128 - ((_x - Globals.cam_x) * distance)
+	
+	if global_position.y > limit: queue_free()
+
+func _crash() -> void:
+	speed = 0
