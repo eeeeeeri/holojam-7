e95ac90a725a89a9011ac01341d95a2ea3105954
Henry Kinoshita <42702870+eeeeeeri@users.noreply.github.com>
2026-01-12 02:19:03 -0300

    gauntlet to graduation transition

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -12,13 +12,13 @@ var all_minigames = [
 	Minigame.minigame("res://Scenes/Minigames/DrivingInMyCar/driving_in_my_car.tscn", "Don't crash","Driving in my car right after a stream")
 ]
 var minigames = [
-	#Minigame.minigame("res://Scenes/Minigames/SuperchatReading/superchat_reading.tscn","Superchat Reading","Read all the Superchats!"),
-	#Minigame.minigame("res://Scenes/Minigames/Rugpull/rugpull.tscn","Scam with $KRONII","Sell $KRONII at a High!"),
-	#Minigame.minigame("res://Scenes/Minigames/SpellingBee/spelling_bee.tscn","Spelling Bee","Is actually typing"),
-	#Minigame.minigame("res://Scenes/Minigames/AddressingAllegations/addressing_allegations.tscn", "Adressing the Allegations","Spot the false allegations"),
-	#Minigame.minigame("res://Scenes/Minigames/OutfitReveal/outfit_reveal.tscn", "New Outfit Reveal","Showcase your new outfit!"),
-	#Minigame.minigame("res://Scenes/Minigames/Subway/subway.tscn", "Eat Subway","Devour it whole"),
-	#Minigame.minigame("res://Scenes/Minigames/DrivingInMyCar/driving_in_my_car.tscn", "Don't crash","Driving in my car right after a stream")
+	Minigame.minigame("res://Scenes/Minigames/SuperchatReading/superchat_reading.tscn","Superchat Reading","Read all the Superchats!"),
+	Minigame.minigame("res://Scenes/Minigames/Rugpull/rugpull.tscn","Scam with $KRONII","Sell $KRONII at a High!"),
+	Minigame.minigame("res://Scenes/Minigames/SpellingBee/spelling_bee.tscn","Spelling Bee","Is actually typing"),
+	Minigame.minigame("res://Scenes/Minigames/AddressingAllegations/addressing_allegations.tscn", "Adressing the Allegations","Spot the false allegations"),
+	Minigame.minigame("res://Scenes/Minigames/OutfitReveal/outfit_reveal.tscn", "New Outfit Reveal","Showcase your new outfit!"),
+	Minigame.minigame("res://Scenes/Minigames/Subway/subway.tscn", "Eat Subway","Devour it whole"),
+	Minigame.minigame("res://Scenes/Minigames/DrivingInMyCar/driving_in_my_car.tscn", "Don't crash","Driving in my car right after a stream")
 ]
 
 var week_minigames = []
@@ -32,6 +32,7 @@ var days_left : int = 14
 var today := WeekDay.MON
 var cam_x : float
 var txt_spd := 0.06
+var game_ended := false
 
 var gauntlet := false
 var gauntlet_minigames = []
